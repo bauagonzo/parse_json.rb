d8d554b044d6d429efb48ea895ccbd2bd414ccef
baua.gonzo <baua.gonzo@gmail.com>
2016-06-06 15:13:56 +0200

    Remove unused variable Cosmetic changes

--- parse_json.rb
+++ parse_json.rb
@@ -2,7 +2,7 @@
 require 'json'
 require 'optparse'
 
-options = {interactive: false}
+options = { interactive: false }
 op = OptionParser.new do |opts|
   opts.banner = "Usage: parse_json.rb [options] 'data'"
   opts.on("-c 'ruby code'", "--code 'ruby'", "code to be executed ; json is in @data variable") do |c|
@@ -29,19 +29,17 @@ end
 
 @raw = ARGV.empty? ? ARGF.read : File.read(ARGV.shift)
 @data = JSON.parse(@raw)
-ta = JSON.parse(@raw)
 
 if options[:code]
   eval(options[:code])
 elsif options[:interactive]
-  #ARGF pry issue : http://stackoverflow.com/questions/32333962/pry-not-stopping-when-called-from-a-ruby-script-that-reads-from-stdin
-  pry_fd_stdin = IO.sysopen("/dev/tty")
-  pry_stdin = IO.new(pry_fd_stdin, "r")
+  # ARGF pry issue : http://stackoverflow.com/questions/32333962/pry-not-stopping-when-called-from-a-ruby-script-that-reads-from-stdin
+  pry_fd_stdin = IO.sysopen('/dev/tty')
+  pry_stdin = IO.new(pry_fd_stdin, 'r')
   Pry.config.input = pry_stdin
-  Pry.output.puts "
+  Pry.output.puts '
   The JSON hash is available in @data
-  The input string in @raw"
-#  binding.pry
+  The input string in @raw'
   Pry.start
 else
   puts @raw
